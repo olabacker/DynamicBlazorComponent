9245a8c5d0c137969b8a744056951d8ed6a0f1d1
Ola Bäcker <olabacker@gmail.com>
2020-09-13 14:03:18 +0200

    initial

---
+++ DynamicComponent/DynamicComponentAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicComponent
+{
+    public class DynamicComponentAttribute
+    {
+        public int Sequence { get; set; }
+
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+    }
+}
